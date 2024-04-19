b31637d555c9c118ea6547c660d854b526af516d
Hammad Hussain <93250@gcslahore.edu.pk>
2024-04-19 11:51:48 +0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import SignBoard from "./components/SignBoard"
+
+const page=()=>{
+  return(
+    <div className="h-screen w-screen flex items-center justify-center">
+      <SignBoard/>
+
+    </div>
+  )
+}
+export default page
